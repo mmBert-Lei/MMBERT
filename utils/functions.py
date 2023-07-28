41e6412d1ce36776ae8e781c7cc407cdaff877ad
mmBert-Lei <140824896+mmBert-Lei@users.noreply.github.com>
2023-07-28 22:16:18 +0800

    Add files via upload

---
+++ utils/functions.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from transformers import BertTokenizer
+
+NULLKEY = "-null-"
+
+
+def normalize_word(word):
+    new_word = ""
+    for char in word:
+        if char.isdigit():
+            new_word += '0'
+        else:
+            new_word += char
+    return new_word
+
+
+bert_name = 'bert-base-chinese'
+tokenizer = BertTokenizer.from_pretrained(bert_name)
+
+
+def token2id(token):
+    return tokenizer.convert_tokens_to_ids(token)
+
+
+dis2idx = np.zeros((1000), dtype='int64')
+dis2idx[1] = 1
+dis2idx[2:] = 2
+dis2idx[4:] = 3
+dis2idx[8:] = 4
+dis2idx[16:] = 5
+dis2idx[32:] = 6
+dis2idx[64:] = 7
+dis2idx[128:] = 8
+dis2idx[256:] = 9
+
+
+class Vocabulary(object):
+    PAD = '<pad>'
+    UNK = '<unk>'
+    SUC = '<suc>'
+
+    def __init__(self):
+        self.label2id = {self.PAD: 0, self.SUC: 1}
+        self.id2label = {0: self.PAD, 1: self.SUC}
+
+    def add_label(self, label):
+        label = label.lower()
+        if label not in self.label2id:
+            self.label2id[label] = len(self.label2id)
+            self.id2label[self.label2id[label]] = label
+
+        assert label == self.id2label[self.label2id[label]]
+
+    def __len__(self):
+        return len(self.token2id)
+
+    def label_to_id(self, label):
+        label = label.lower()
+        return self.label2id[label]
+
+    def id_to_label(self, i):
+        return self.id2label[i]
+
+def build_pretrain_embedding(embedding_path, word_alphabet, embedd_dim=100, norm=True):
+    embedd_dict = dict()
+    if embedding_path != None:
+        embedd_dict, embedd_dim = load_pretrain_emb(embedding_path)
+    scale = np.sqrt(3.0 / embedd_dim)
+    pretrain_emb = np.empty([word_alphabet.size(), embedd_dim])
+    perfect_match = 0
+    case_match = 0
+    not_match = 0
+    for word, index in word_alphabet.iteritems():
+        if word in embedd_dict:
+            if norm:
+                pretrain_emb[index, :] = norm2one(embedd_dict[word])
+            else:
+                pretrain_emb[index, :] = embedd_dict[word]
+            perfect_match += 1
+        elif word.lower() in embedd_dict:
+            if norm:
+                pretrain_emb[index, :] = norm2one(embedd_dict[word.lower()])
+            else:
+                pretrain_emb[index, :] = embedd_dict[word.lower()]
+            case_match += 1
+        else:
+            pretrain_emb[index, :] = np.random.uniform(-scale, scale, [1, embedd_dim])
+            not_match += 1
+    pretrained_size = len(embedd_dict)
+    print("Embedding:\n     pretrain word:%s, prefect match:%s, case_match:%s, oov:%s, oov%%:%s" % (
+        pretrained_size, perfect_match, case_match, not_match, (not_match + 0.) / word_alphabet.size()))
+    return pretrain_emb, embedd_dim
+
+
+def norm2one(vec):
+    root_sum_square = np.sqrt(np.sum(np.square(vec)))
+    return vec / root_sum_square
+
+
+def load_pretrain_emb(embedding_path):
+    embedd_dim = -1
+    embedd_dict = dict()
+    with open(embedding_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            line = line.strip()
+            if len(line) == 0:
+                continue
+            tokens = line.split()
+            if embedd_dim < 0:
+                embedd_dim = len(tokens) - 1
+            else:
+                assert (embedd_dim + 1 == len(tokens))
+            embedd = np.empty([1, embedd_dim])
+            embedd[:] = tokens[1:]
+            # embedd_dict[tokens[0].decode('utf-8')] = embedd
+            embedd_dict[tokens[0]] = embedd
+    return embedd_dict, embedd_dim
+
+
+if __name__ == '__main__':
+    a = np.arange(9.0)
